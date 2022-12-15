371c510dab01b03b2ec978ea32b8378aeabca61a
ktyoung <ktyoung0507@gmail.com>
2022-12-15 15:11:46 +0900

    JSX 문법

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '안양 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* class 명은 className으로 선언 */}
+      <div className='black-nav'>
+        {/* style 넣으려면 style={ {스타일명:'값'} } */}
+        {/* 스타일명은 CamelCase로 작성 */}
+        <h4 style={ {color : 'red', fontSize : '20px' } }>리액트 블로그</h4>
+      </div>
+      {/* 중괄호를 사용하면 데이터 바인딩이 아주 편리함 */}
+      {/* 변수를 사용할 땐 { 변수명 } */}
+      <h4>{ post }</h4>
     </div>
   );
 }
