db37fdc47a3635ab172772cf35f78fa0ea102ffb
ktyoung <ktyoung0507@gmail.com>
2022-12-17 17:56:05 +0900

    props

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,12 +10,13 @@ function App() {
   let post = '안양 맛집';
   
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [likeCount, setLikeCount] = useState([0, 0, 0]);
+  function rename() {
+    let copy = [...글제목];
+    copy[0] = "여자 코트 추천";
+    글제목변경(copy);
+  }
 
-  // map : 반복문
-  // 1. array의 자료 개수만큼 함수 내부 코드를 실행함
-  // 2. 함수의 매개변수는 array 안에 있던 자료
-  // 3. return 값을 array로 담아줌
+  let [likeCount, setLikeCount] = useState([0, 0, 0]);
 
   let [modal, setModal] = useState(false);
   function toggleModal() {
@@ -32,74 +33,47 @@ function App() {
         <h4>{ logo }</h4>
       </div>
 
-      {/* <button className='sort-btn' 
-        onClick={ () => {
-          let copy = [...글제목];
-          copy.sort();
-          글제목변경(copy)
-        } }>제목 정렬</button>
-
-      <div className='list'>
-        <button onClick={ () => { 
-          let copy = [...글제목];
-          copy[0] = "여자 코트 추천";
-          글제목변경(copy);
-          } }>제목 변경</button>
-        <h4>{ 글제목[0] }</h4>
-        <p>12월 15일 발행</p>
-      </div>
-
-      <div className='list'>
-        <h4>{ 글제목[1] }</h4>
-        <p>12월 15일 발행</p>
-      </div>
-
-      <div className='list'>
-        <h4>{ 글제목[2] }</h4>
-        <p>12월 15일 발행</p>
-      </div> */}
-
-      {/* 같은 html 반복 생성하는 법 */}
-      {/* state의 크기 == 블로그의 글 개수 */}
-      {/* 1. 매개변수로 i를 전달하면 서로 다른 블로그 글이 출력됨 */}
-      {/* 2. 두 번째 매개변수는 반복문을 수행할 때마다 0부터 1씩 증가하는 정수 : 0 → 1 → 2 → ... */}
       {
-        // 글제목의 개수만큼 반복
-        // return 값을 array로 반환
-        // 매개변수로 2개 사용 가능
         글제목.map(function(a, i){
           return (
-            // 반복문으로 html 생성하면 key={ html마다 다른 숫자 } 추가해야함 → 반복 생성한 UI가 각각 다른 key 값을 가져야함
             <div className='list' key={ i }>
               <h4 onClick={ toggleModal }>{ a } 
-              {/* ※※※ array 자료 변경 시에는 copy한 후 copy본을 수정 ※※※ */}
               <span onClick={ () => { 
                 let copy = [...likeCount];
                 copy[i] += 1;
                 setLikeCount(copy);
                } }>👍
               </span> { likeCount[i] } </h4>
-              {console.log(likeCount[i])}
-              {/* <h4>{ 글제목[i] }</h4> → 매개변수 i를 이용해도 출력 가능하다 */}
               <p>12월 15일 발행</p>
             </div>
           )
         })
       }
 
+      {/* props 문법 사용 방법 (부모 → 자식 state 전송하는 법) */}
+      {/* 1. <자식컴포넌트 작명={state이름} /> */}
       {
-        modal == true ? <Modal/> : null
+        // (참고) props로 문자도 전송 가능
+        modal == true ? <Modal 글제목={글제목} color="skyblue" rename={rename} /> : null
       }
     </div>
   );
 }
 
-function Modal() {
+// state가 function App() {}에 정의되어 있으므로 state를 임의로 사용 불가함
+// props 문법을 사용하면 자식이 부모의 state를 사용 가능함
+// props 전송은 부모 → 자식만 가능함!
+
+// 2. props 매개변수 등록 후 props.작명 사용
+function Modal(props) {
   return (
-    <div className='modal'>
-      <h4>제목</h4>
+    // 다양한 색의 모달창이 필요하다면?
+    // background 스타일을 props로 받아와 사용(함수의 매개변수처럼 활용 가능)
+    <div className='modal' style={{background : props.color}}>
+      <h4>{ props.글제목[0] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={ () => { props.rename() } }>글수정</button>
     </div>
   );
 }
