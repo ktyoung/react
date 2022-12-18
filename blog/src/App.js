890e26fb714f4b528b5fb714b61d355b7cdc0d34
ktyoung <ktyoung0507@gmail.com>
2022-12-18 22:16:10 +0900

    input

--- blog/src/App.js
+++ blog/src/App.js
@@ -20,6 +20,8 @@ function App() {
 
   let [title, setTitle] = useState(0);
 
+  let [inputText, setInputText] = useState('');
+
   let [modal, setModal] = useState(false);
   function toggleModal() {
     if (modal == true) {
@@ -29,6 +31,16 @@ function App() {
     }
   }
 
+  // 새로운 글 추가하기
+  function addText() {
+    글제목변경(글제목.concat(inputText))
+  }
+
+  // 기존 글 삭제하기
+  function removeText(key) {
+    let newArr = 글제목.filter(a => a !== key)
+    글제목변경(newArr);
+  }
 
   return (
     <div className="App">
@@ -37,21 +49,42 @@ function App() {
       </div>
 
       {
+        // 왜 <span> 태그를 눌러도 모달창이 뜨는가?
+        // 이벤트 버블링(클릭 이벤트는 상위 html로 퍼짐) 때문
+        // ※※※ 이벤트 버블링을 방지하려면 : e.stopPropagation() ※※※ //
         글제목.map(function(a, i){
           return (
             <div className='list' key={ i }>
               <h4 onClick={ () => { toggleModal(); setTitle(i); }}>{ a } 
-              <span onClick={ () => { 
+              <span onClick={ (e) => { 
+                e.stopPropagation();
                 let copy = [...likeCount];
                 copy[i] += 1;
                 setLikeCount(copy);
                } }>👍
               </span> { likeCount[i] } </h4>
               <p>12월 15일 발행</p>
+              <button className='del-btn' 
+                onClick={ () => { removeText(a) } }>삭제</button>
             </div>
           )
         })
       }
+
+      {/* <input> 태그 */}
+      {/* 1. 다양한 type이 있음 */}
+      {/* 2. <input> 태그에 뭔가 입력 시 코드를 실행하고 싶으면 onChange / onInput */}
+      {/* 3. 매우 다양한 이벤트 핸들러가 있음. onMouseOver, onScroll 등등... */}
+      {/* 4. <input>에 입력한 값 가져오는 방법? */}
+      {/* 4-1. 매개변수로 e 전달 → e.target.value */}
+      {/* 5. <input>에 입력된 값을 저장하려면? */}
+      {/* 5-1. state 변경 함수를 사용해 저장한다 */}
+      <input onChange={ (e) => { setInputText(e.target.value); } } />
+      <button onClick={ addText }>글발행</button>
+
+      {/* (참고 1) dropdown 박스는 <select> 태그 */}
+      {/* (참고 2) 긴 텍스트 입력 시에는 <textarea> 태그 */}
+      {/* (참고 3) state 변경 함수는 늦게 처리됨(비동기처리) */}
 
       {
         modal == true ? <Modal 글제목={글제목} rename={rename} title={title} /> : null
@@ -61,9 +94,6 @@ function App() {
 }
 
 function Modal(props) {
-  // state를 자식 컴포넌트에 생성해도 됨
-  // 그러나, state가 다양한 컴포넌트에 필요하면 최상위 컴포넌트(App)에 생성해야함
-  // let [title, setTitle] = useState(0);
   return (
     <div className='modal'>
       <h4>{ props.글제목[props.title] }</h4>
