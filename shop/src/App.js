7f56ee83f2bd3d8852e04804836402fcc39861ca
ktyoung <ktyoung0507@gmail.com>
2022-12-21 20:17:22 +0900

    navigate, nested routes, outlet

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,42 +1,31 @@
 /* eslint-disable */
 
-// (참고 1) import 시 본인이 만든 파일은 './경로'의 형태
-// 라이브러리의 경우 라이브러리 명만 입력되어 있음
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import React, { useState } from 'react';
 import data from './data.js';
-// react-route-dom 컴포넌트 사용을 위해 import
-import { Routes, Route, Link } from 'react-router-dom'
-
-// html에서 다른 페이지로 이동하려면 다른 html을 연결했음
-// 리액트에서 다른 페이지로 이동하려면?
-// 1. 다른 페이지의 컴포넌트를 생성
-// 2. 해당 페이지로 접속 시 1의 컴포넌트를 보여줌
-
-// (참고 2) react-router-dom 라이브러리를 사용하면 쉽게 사용 가능함
-// 1. 터미널에 npm install react-router-dom@6 입력(6버전)
-// 2. index.js에 <BrowserRouter> 코드 추가
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './routes/Detail.js';
 
 function App() {
 
   let [shoes] = useState(data);
 
+  // 페이지 이동을 도와주는 내부 함수가 내장된 useNavigate()
+  let navigate = useNavigate();
+
   return (
     <div className="App">
-      {/* (참고 3) Route 컴포넌트? */}
-      {/* 1. <Route />는 페이지와 같음 */}
-        {/* 1-1. <Route path="/url경로" element={ <출력할 html> } /> */}
-        {/* 1-2. 메인 페이지는 path="/" */}
-      
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            {/* 페이지 이동 버튼은 <Link to="경로"></Link> */}
-            <Link className='link' to="/">홈</Link>
-            <Link className='link' to="/detail">상세페이지</Link>
+            {/* navigate('경로')로 원하는 링크로 이동하게 설정할 수 있다 */}
+            {/* navigate(정수)를 입력하면 입력한 수만큼 뒤로가기/앞으로 가기 기능 설정 가능 
+                  예) navigate(1) 1페이지 앞으로, navigate(-2) 2페이지 뒤로 */}
+            <Nav.Link className='link' onClick={ () => { navigate('/') } }>홈</Nav.Link>
+            <Nav.Link className='link' onClick={ () => { navigate('/detail') } }>상세페이지</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -60,7 +49,54 @@ function App() {
           </>
         } />
         <Route path="/detail" element={ <Detail/> } />
+
+        {/* path="*"은 위에 작성한 페이지 외 모든 페이지를 의미함(오류 페이지) */}
+        <Route path="*" element={ <div>존재하지 않는 페이지</div> } />
+
+        {/* 
+          서브 경로를 만들 때 유용한 Nested Routes 
+          /about/member(location) 경로와 같음 
+        */}
+
+        {/* 
+        (참고 1) Nested Routes의 장점?
+        1. route 작성이 간단해짐
+        2. 부모 element와 본인의 element, 총 2개의 element를 화면에 출력함 
+          2-1. 정상적으로 출력하려면 <About> 컴포넌트 안에 <div>를 어디에 출력할 지 표기해야 함 
+                → 출력 위치를 정하는 것이 <Outlet> 
+        3. 여러 유사한 페이지를 작성할 때 사용하면 좋음
+        */}
+        <Route path="/about" element={ <About/> }>
+          <Route path="member" element={ <div>멤버 정보</div> } />
+          <Route path="location" element={ <div>위치 정보</div> } />
+        </Route>
+
+        <Route path="/event" element={ <Event/> }>
+          <Route path="one" element={ <p>첫 주문 시 신발끈 서비스</p> } />
+          <Route path="two" element={ <p>생일 기념 쿠폰 받기</p> } />
+        </Route>
+
       </Routes>
+    </div>
+  );
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      {/* Nested Routes의 출력 위치를 결정하는 <Outlet></Outlet> */}
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사 정보</h4>
+      {/* Nested Routes의 출력 위치를 결정하는 <Outlet></Outlet> */}
+      <Outlet></Outlet>
     </div>
   );
 }
@@ -73,24 +109,6 @@ function Card(props) {
       <p>{ props.shoes.content }</p>
       <p>{ props.shoes.price }</p>
     </Col>
-  );
-}
-
-function Detail() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-        </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button> 
-        </div>
-      </div>
-    </div> 
   );
 }
 
