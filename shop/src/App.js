434e52f71a1c0fc0951394b2540a5658e6afe999
ktyoung <ktyoung0507@gmail.com>
2022-12-21 21:20:50 +0900

    URL 파라미터

--- shop/src/App.js
+++ shop/src/App.js
@@ -11,8 +11,6 @@ import Detail from './routes/Detail.js';
 function App() {
 
   let [shoes] = useState(data);
-
-  // 페이지 이동을 도와주는 내부 함수가 내장된 useNavigate()
   let navigate = useNavigate();
 
   return (
@@ -21,9 +19,6 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            {/* navigate('경로')로 원하는 링크로 이동하게 설정할 수 있다 */}
-            {/* navigate(정수)를 입력하면 입력한 수만큼 뒤로가기/앞으로 가기 기능 설정 가능 
-                  예) navigate(1) 1페이지 앞으로, navigate(-2) 2페이지 뒤로 */}
             <Nav.Link className='link' onClick={ () => { navigate('/') } }>홈</Nav.Link>
             <Nav.Link className='link' onClick={ () => { navigate('/detail') } }>상세페이지</Nav.Link>
           </Nav>
@@ -48,24 +43,17 @@ function App() {
           </Container>
           </>
         } />
-        <Route path="/detail" element={ <Detail/> } />
 
-        {/* path="*"은 위에 작성한 페이지 외 모든 페이지를 의미함(오류 페이지) */}
+        {/* props 문법을 이용 → App()에서 Detail()로 데이터 바인딩 */}
+        {/* 100개의 상품 페이지가 필요하다면? :URL 파라미터 사용! 
+              → /detail/아무거나 라는 의미... ":작명" */}
+
+        {/* (참고) url 파라미터는 여러개 설정 가능하다 
+              예) /detail/:id/:color/... */}
+        <Route path="/detail/:id" element={ <Detail shoes={ shoes } /> } />
+
         <Route path="*" element={ <div>존재하지 않는 페이지</div> } />
 
-        {/* 
-          서브 경로를 만들 때 유용한 Nested Routes 
-          /about/member(location) 경로와 같음 
-        */}
-
-        {/* 
-        (참고 1) Nested Routes의 장점?
-        1. route 작성이 간단해짐
-        2. 부모 element와 본인의 element, 총 2개의 element를 화면에 출력함 
-          2-1. 정상적으로 출력하려면 <About> 컴포넌트 안에 <div>를 어디에 출력할 지 표기해야 함 
-                → 출력 위치를 정하는 것이 <Outlet> 
-        3. 여러 유사한 페이지를 작성할 때 사용하면 좋음
-        */}
         <Route path="/about" element={ <About/> }>
           <Route path="member" element={ <div>멤버 정보</div> } />
           <Route path="location" element={ <div>위치 정보</div> } />
@@ -85,7 +73,6 @@ function Event() {
   return (
     <div>
       <h4>오늘의 이벤트</h4>
-      {/* Nested Routes의 출력 위치를 결정하는 <Outlet></Outlet> */}
       <Outlet></Outlet>
     </div>
   );
@@ -95,7 +82,6 @@ function About() {
   return (
     <div>
       <h4>회사 정보</h4>
-      {/* Nested Routes의 출력 위치를 결정하는 <Outlet></Outlet> */}
       <Outlet></Outlet>
     </div>
   );
--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,17 +1,25 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+
+    // 유저가 URL 파라미터에 입력한 값을 가져오려면 useParams() 사용
+    // 경로 뒤 작명한 이름을 가져옴
+    let {id} = useParams();
+
     return (
         <div className="container">
-        <div className="row">
-            <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={"https://codingapple1.github.io/shop/shoes"+ (parseInt(id)+1) +".jpg"} width="100%" />
+                </div>
+                <div className="col-md-6">
+                    {/* props.shoes[현재 URL에 입력된 숫자]로 설정하면 여러 페이지 출력 가능 */}
+                    <h4 className="pt-5">{ props.shoes.find(shoes => shoes.id === parseInt(id)).title }</h4>
+                    <p>{ props.shoes.find(shoes => shoes.id === parseInt(id)).content }</p>
+                    <p>{ props.shoes.find(shoes => shoes.id === parseInt(id)).price }원</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                </div>
             </div>
-            <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
-            <button className="btn btn-danger">주문하기</button> 
-            </div>
-        </div>
         </div> 
     );
 }
