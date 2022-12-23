307f5357a8af5e01dcff68bf726fd1183d6b2ece
ktyoung <ktyoung0507@gmail.com>
2022-12-23 19:32:52 +0900

    animation

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -2,31 +2,31 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {Nav} from 'react-bootstrap';
 
+// 애니메이션 효과 주는 방법
+// 1. 애니메이션 동작 전 스타일을 담을 className 만들기 
+// 2. 애니메이션 동작 후 스타일을 담을 className 만들기 
+// 3. transition 속성 추가
+// 4. 원할 때 CSS 탈부착
+
 function Detail(props) {
     let {id} = useParams();
-    let input = document.getElementsByClassName('getText');
 
     let [count, setCount] = useState(0);
     let [notice, setNotice] = useState(true);
     let [tabs, setTabs] = useState(0);
-    // let [inputText, setInputText]  = useState('');
+    let [fadeDetail, setFadeDetail] = useState('');
 
     useEffect( () => {
         let timer = setTimeout( () => { setNotice(false)  }, 2000 )
-
-        // if (isNaN(inputText)) {
-        //     setInputText(inputText = '');
-        //     input.value = inputText;
-        //     alert("숫자만 입력하세요.");
-        //     console.log(input.value);
-        // }
+        let fadeAnimateDetail = setTimeout( () => {setFadeDetail('end');}, 100 );
         return () => {
             clearTimeout(timer);
+            clearTimeout(fadeAnimateDetail);
         }
-    }, [count])
+    }, [])
 
     return (
-        <div className="container">
+        <div className={"container start " + fadeDetail}>
             {
                 notice == true 
                 ?   <div className="alert alert-warning">
@@ -40,7 +40,6 @@ function Detail(props) {
                     <img src={"https://codingapple1.github.io/shop/shoes"+ (parseInt(id)+1) +".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
-                    {/* <input className="getText" type="text" value={ inputText } onChange={ (e) => { setInputText(e.target.value) } } /> */}
                     <h4 className="pt-5">{ props.shoes.find(shoes => shoes.id === parseInt(id)).title }</h4>
                     <p>{ props.shoes.find(shoes => shoes.id === parseInt(id)).content }</p>
                     <p>{ props.shoes.find(shoes => shoes.id === parseInt(id)).price }원</p>
@@ -48,9 +47,7 @@ function Detail(props) {
                 </div>
             </div>
 
-            {/* eventKey 속성은 버튼마다 마음대로 작명하면 됨
-            defaultActiveKey 속성은 페이지 로드 시 어떤 버튼을 클릭한 효과를 줄 지 결정하는 부분 */}
-            <Nav variant="tabs" defaultActiveKey="link-0">
+            <Nav className="tab-ui" variant="tabs" defaultActiveKey="link-0">
                 <Nav.Item>
                     <Nav.Link eventKey="link-0" onClick={ () => { setTabs(0) } }>버튼 0</Nav.Link>
                 </Nav.Item>
@@ -62,40 +59,38 @@ function Detail(props) {
                 </Nav.Item>
             </Nav>
 
-            {/* 컴포넌트화 한 조건문을 사용 */}
             <TabContents tabs={tabs}/>
             
         </div> 
     );
 }
 
-// 조건문 사용하기 → 컴포넌트화 하여 사용
-function TabContents(props) {
-    if (props.tabs == 0) {
-        return <div>내용0</div>
-    } else if (props.tabs == 1) {
-        return <div>내용1</div>
-    } else {
-        return <div>내용2</div>
-    }
+function TabContents({tabs}) {
+
+    let [fade, setFade] = useState('');
+
+    // tabs state가 변할 때마다 'end' 클래스를 부착 → useEffect() 사용
+    useEffect( () => {
+        // (참고 1) setTimeout으로 클래스명 부착에 지연시간을 줘야 애니메이션이 동작하는 이유?
+        // 1. 리액트의 automatic batching 기능 때문에 마지막 state 변경 함수만 동작함
+        //  1-1. setFade('') → setFade('end') 순서로 실행되는 것이 아니라 setFade('end')만 동작함
+        // 2. 따라서, 클래스명 부착을 나중에 하게되면 정상적으로 동작함
+        let fadeAnimate = setTimeout( () => {setFade('end');}, 100 );
+
+        // clean up function(useEffect 실행 전에 동작하는 함수)
+        return () => {
+            clearTimeout(fadeAnimate);
+            setFade('');
+        }
+    }, [tabs])
+
+    return (
+        // 4. 애니메이션 CSS 탈부착
+        // `문자 ${변수}`로 html 사이에 문자 삽입 가능
+        <div className={"start " + fade}> 
+            { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tabs] }
+        </div>
+    );
 }
-
-// (참고 1) props 등록 및 사용이 번거로우면?
-// props 대신 {변수명}을 파라미터로 사용
-// function TabContents({tabs}) {
-//     if (tabs == 0) {
-//         return <div>내용0</div>
-//     } else if (tabs == 1) {
-//         return <div>내용1</div>
-//     } else {
-//         return <div>내용2</div>
-//     }
-// }
-
-// (참고 2) if문 대신 사용할 수 있는 방법?
-// html을 배열로 설정하여 인덱싱[tabs]하는 방법
-// function TabContents({tabs}) {
-//    return [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tabs]
-// }
 
 export default Detail;
